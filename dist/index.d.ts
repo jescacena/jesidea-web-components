c5fa33392fb36e2668bf9a7b3ec25a42480e8421
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2020-08-23 15:13:45 +0200

    Version 0.0.11 : fix release workflow

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,5 @@
 export { MyElement } from "./my-element";
+export { JwcProjectCard } from "./jwc-project-card/jwc-project-card";
+export { JwcUserProfile } from "./jwc-user-profile/jwc-user-profile";
+export { JwcQuoteRandomBox } from "./jwc-quote-random-box/jwc-quote-random-box";
 //# sourceMappingURL=index.d.ts.map
